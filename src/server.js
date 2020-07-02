a18cd0bbcd3c3598064601a65f15de5b9234e00e
MyDougie <01046009100@hanmail.net>
2020-07-02 22:32:16 +0900

    Reconstructor Types and Resolvers

--- src/server.js
+++ src/server.js
@@ -1,21 +1,14 @@
 //.env file load
 require("dotenv").config();
 import { GraphQLServer } from "graphql-yoga";
+import logger from "morgan";
+import schema from "./schema";
 
 const PORT = process.env.PORT || 4000;
 
-const typeDefs = `
-    type Query{
-        hello(name: String): String!
-    }
-`;
-const resolvers = {
-  Query: {
-    hello: (_, { name }) => `Hello ${name || "World"}`,
-  },
-};
+const server = new GraphQLServer({ schema });
 
-const server = new GraphQLServer({ typeDefs, resolvers });
+server.express.use(logger("dev"));
 
 server.start({ port: PORT }, () =>
   console.log(`Server running on http://localhost:${PORT}`)
